1dfdb74d378dc9cec2cf9ad4b58b2cfd124a9e9a
Tobias Bogarin <tobiasebogarin@gmail.com>
2025-08-16 01:44:55 -0300

    fin

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-// server.js
+// server.js en raíz
 const express = require("express");
 const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Servir archivos estáticos desde /public
-app.use(express.static(path.join(__dirname, "public")));
+// Servir estáticos desde /src/public
+app.use(express.static(path.join(__dirname, "src", "public")));
 
 app.get("/", (_req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+  res.sendFile(path.join(__dirname, "src", "public", "index.html"));
 });
 
 app.listen(PORT, () => {
