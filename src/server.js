6528b687f0ce39c48803a55a587afacf9d1fedff
Tobias Bogarin <tobiasebogarin@gmail.com>
2025-08-16 01:09:41 -0300

    re estructurado

---
+++ src/server.js
@@ -0,0 +1,17 @@
+// src/server.js
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Servir /src/public
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (_req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`🚀 Portfolio server running on http://localhost:${PORT}`);
+});
